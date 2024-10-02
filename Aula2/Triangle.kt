fbabae333014440c93d70391b9dd4a5ae33baf91
Pedro Pereira <593033+palex65@users.noreply.github.com>
2024-10-02 10:56:15 +0100

    Aula2 (start)
    Aula de 2 Outubro

---
+++ Aula2/Triangle.kt
@@ -0,0 +1,26 @@
+/* Triangle.kt
+    Escreva um programa que classifica um triângulo dado o comprimento de cada lado.
+    O programa deve executar as seguintes ações:
+    1. Questionar o utilizador sobre o lado A, B e C (Valor Inteiro)
+    2. Apresentar uma das quatro possíveis classificações do triângulo no formato:
+           O triângulo é <Impossível|Equilátero|Isósceles|Escaleno>
+    3. Caso o triângulo seja retângulo apresentar também:
+           O triângulo é retângulo
+ */
+
+fun main() {
+    print("Lado A? ")
+    val a = readln().trim().toInt()
+    print("Lado B? ")
+    val b = readln().trim().toInt()
+    print("Lado C? ")
+    val c = readln().trim().toInt()
+
+
+
+
+
+
+
+
+}
